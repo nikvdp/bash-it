3c36b6d0c2b39842f95a168bd6a241cd9d815a24
Nik V <nik@nikvdp.com>
2017-02-24 19:31:18 -0800

    fix ssh-agent plugin for os x

--- plugins/available/ssh-agent.plugin.bash
+++ plugins/available/ssh-agent.plugin.bash
@@ -1,29 +1,38 @@
-# cite ssh-agent-plugin
-# about-plugin 'Automatically start ssh-agent'
+cite ssh-agent-plugin
+about-plugin 'Automatically start ssh-agent'
 
 
 load-fingerprints-and-ids () {
     export fingerprints=
 
     while read id_file; do
-        fingerprints="\"$id_file\" $(key-to-fingerprint "$id_file")$fingerprints"
+        fingerprints="$id_file $(key-to-fingerprint "$id_file")\n$fingerprints"
     done < <(ls ~/.ssh/id* | grep -v '\.pub$')
 
-    echo -e $fingerprints
+    echo -e $fingerprints | sed '/^$/d'
 }
 
 key-to-fingerprint() {
-    local key="$1"
-    ssh-keygen -lf "$key" | cut -d ' ' -f 2
+    local id_file="$1"
+    if [[ ! -f "$id_file.pub" ]]; then
+        get-public-key-from-private "$id_file" > "$id_file.pub"
+        chmod 600 "$id_file.pub"
+    fi
+    ssh-keygen -lf "$id_file.pub" | cut -d ' ' -f 2
+}
+
+get-public-key-from-private() {
+    local id_file="$1"
+    ssh-keygen -yf "$id_file" 
 }
 
 get-agent-fingerprints() {
-    ssh-add -l 2>/dev/null | cut -d ' ' -f 2
+    ssh-add -l | cut -d ' ' -f 2
 }
 
 
 start-agent-if-unstarted() {
-    if [[ -z "$SSH_AGENT_PID" ]] && ps ax | grep -q $SSH_AGENT_PID &> /dev/null; then
+    if ! ssh-add -l &>/dev/null; then
         eval "$(ssh-agent)"
     fi
 }
@@ -33,13 +42,25 @@ add-identities () {
         set -- $fingerprint_and_id
         local id_file="$1"
         local fingerprint="$2"
+        # echo "Fingerprint=[$fingerprint], id_file=[$id_file]"
         if ! get-agent-fingerprints | grep -q $fingerprint; then
-            ssh-add $id_file
+            local optional_params=
+            if [[ "$(uname)" == "Darwin" ]]; then
+                optional_params="-K "
+            fi
+            ssh-add $optional_params $id_file
         fi
     done
+
+    # re-run ssh-add, so that it's exit code becomes this function's
+    # exit code. if ssh-add's exit code is non-zero that means that either
+    # we didn't add any ids or that ssh-add couldn't connect to the agent
+    ssh-add -l &> /dev/null
 }
 
 
-start-agent-if-unstarted
-add-identities
+# if agent is already running then just try to add identities to it. If that doesn't work
+# then attempt to start it and then add identities
+add-identities || (start-agent-if-unstarted && add-identities)
+
         
